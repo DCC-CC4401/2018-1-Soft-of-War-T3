388b26d644fba8c1eff6cec96780b0e8b69cedc7
23pablo97 <pablo.arancibia@ing.uchile.cl>
2018-05-03 20:31:22 -0300

    Se coloca atributo imagenes al modelo Productos

--- Inventario/views.py
+++ Inventario/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Productos
+
 def index(request):
     return HttpResponse('Aca debe ir el Login')
+
+def productos(request):
+    products = Productos.objects.all()[:10]
+    context = {'products': products}
+    return render(request, 'productos.html', context)

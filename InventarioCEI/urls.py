0c68d99b2d1a98168a1285d7f6ec0e5b77be14fa
23pablo97 <pablo.arancibia@ing.uchile.cl>
2018-05-02 22:00:32 -0300

    Correciones menores

--- InventarioCEI/urls.py
+++ InventarioCEI/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^', include('Inventario.urls')),
+    path('', include('Inventario.urls')),
     path('admin/', admin.site.urls),
 ]
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 2018-1-Soft-of-War-T3
-Python: 3.6
-Django: 2.0
+##Python: 3.6
+##Django: 2.0

333a9ea4e37054ed8d45f166d0796609e4aa5df5
Tomás Ahumada <tomyahu@gmail.com>
2018-05-06 19:05:28 -0300

    horas de referencia agregadas

--- js/grilla/grilla_fondo.js
+++ js/grilla/grilla_fondo.js
@@ -7,7 +7,8 @@ var canvas = d3.select("#abc")
 				.attr("height", height);
 
 var grupo_lineas_base = canvas.append("g");
-var grupo_texto_dias = canvas.append("g").attr("class", "titulo_grilla");
+var grupo_texto_horas = canvas.append("g").attr("class", "horas_grilla");
+var grupo_texto_dias = canvas.append("g").attr("class", "dias_grilla");
 					
 //Lineas Horizontales Horas
 
@@ -38,6 +39,7 @@ for(i = 3; i <= 9; i+=2)
 
 
 // Dias de la Semana
+
 var dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
 for(i = 0; i < 5; i++)
 {
@@ -46,3 +48,15 @@ for(i = 0; i < 5; i++)
 						.attr("y", height/10)
 						.text(dias[i]);
 }
+
+
+//Horas del dia
+
+var horas = ["10:00","12:00","14:00","16:00","18:00"]
+for(i = 2; i <= 6; i++)
+{
+	grupo_texto_horas.append("text")
+					.attr("x", 15*width/192)
+					.attr("y", height*(1 + i*14/6)/20)
+					.text(horas[i-2]);
+}	

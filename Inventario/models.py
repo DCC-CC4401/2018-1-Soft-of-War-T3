afaf4baa7538da148b932bdcf851e51932eb42ca
23pablo97 <pablo.arancibia@ing.uchile.cl>
2018-05-18 16:50:58 -0400

    Se modifican cuadricula de productos para ver el estado de estos

--- Inventario/models.py
+++ Inventario/models.py
@@ -4,5 +4,6 @@ class Productos(models.Model):
     title       = models.CharField(max_length=200)
     description = models.TextField()
     image       = models.ImageField(upload_to='img/photos/', default='img/photos/default.png')
+    status      = models.IntegerField(default=1, choices=((1, 'Habilitado'),(0, 'Deshabilitado')))
     def __str__(self):
         return self.title

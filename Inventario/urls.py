fc7295ca35433694aaa580a569c8ec06eb72b305
Daniel Araya <daniel.arayap1@gmail.com>
2018-05-22 00:33:52 +0000

    testing seleccion de reserva para detalle

--- Inventario/urls.py
+++ Inventario/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='header'),
     path('productos/', views.productos, name='productos'),
-    path('user/', views.user, name='user')    
+    path('user/', views.user, name='user'),
+    path('ex/', views.ex, name='ex'),        
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Inventario/views.py
+++ Inventario/views.py
@@ -23,3 +23,14 @@ def user(request):
     }
     return render(request, 'user.html', context)
 
+def ex(request):
+    reservs = Reserva.objects.all()[:10]
+    products = Productos.objects.all()[:10]
+    loans = Prestamo.objects.all()[:10]
+    context = {
+        'reservs': reservs,
+        'loans': loans,
+        'products': products,
+    }
+    return render(request, 'ex.html', context)
+

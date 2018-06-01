a42897a9d2390d712cddab075d2f5cde753bd3f6
tomyahu <tomyahu@gmail.com>
2018-06-01 17:41:41 -0400

    modelos nuevos agregados (Espacio y Reserva Espacio)

--- Inventario/admin.py
+++ Inventario/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from .models import Productos,Reserva,Prestamo
+from .models import Productos,Reserva,Prestamo,Espacio,ReservaEspacio
 
 admin.site.register(Productos)
 
 admin.site.register(Reserva)
 
 admin.site.register(Prestamo)
+
+admin.site.register(Espacio)
+
+admin.site.register(ReservaEspacio)

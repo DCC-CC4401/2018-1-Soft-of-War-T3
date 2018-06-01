31348c64baf3aae9527381fe53f5c37649ac3818
tomyahu <tomyahu@gmail.com>
2018-06-01 17:41:01 -0400

    ReservaEspacio editado

--- Inventario/models.py
+++ Inventario/models.py
@@ -33,12 +33,33 @@ class Espacio(models.Model):
     def __str__(self):
         return self.name
 
-class Reserva_Espacio(models.Model):
+class ReservaEspacio(models.Model):
     # user should be an object User
     user        = models.CharField(max_length=200, default="No tiene usuario asignado.")
     space       = models.ForeignKey(Espacio, on_delete=models.CASCADE)
     state       = models.CharField(max_length=200)
-    date        = models.DateTimeField('date reserved', null=True, blank=True, default=datetime.now)
+    date_start  = models.DateTimeField('date start', null=True, blank=True, default=datetime.now)
+    date_end    = models.DateTimeField('date finish', null=True, blank=True, default=datetime.now)
 
     def __str__(self):
+        fecha_1 = 0
+        fecha_2 = 0
         return str(self.space)
+
+    def hora_inicial(self):
+        return int(self.date_start.strftime('%H'))
+
+    def hora_final(self):
+        return int(self.date_end.strftime('%H'))
+
+    def minuto_inicial(self):
+        return int(self.date_start.strftime('%M'))
+
+    def minuto_final(self):
+        return int(self.date_end.strftime('%M'))
+
+    def dia_semana(self):
+        return int(self.date_start.strftime('%w'))
+
+    def dia_anho(self):
+        return int(self.date_start.strftime('%j'))
